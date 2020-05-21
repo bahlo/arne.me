d78459e36c2661865749801f107b1b945da41a21
Arne Bahlo <hallo@arne.me>
2020-05-21 11:50:21 +0200

    Add likes page, change link color

--- .eleventy.js
+++ .eleventy.js
@@ -13,13 +13,16 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginInclusiveLang);
 
+  eleventyConfig.addFilter('parseDate', dateStr => {
+    return new Date(dateStr);
+  });
+
   eleventyConfig.addFilter('readableDate', dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('dd LLL yyyy');
   });
 
-
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+  eleventyConfig.addFilter('htmlDateString', dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
   });
 
---
+++ _data/likes.js
@@ -0,0 +1,9 @@
+const Cache = require('@11ty/eleventy-cache-assets');
+
+const USERNAME = process.env.PINBOARD_USERNAME;
+const PASSWORD = process.env.PINBOARD_PASSWORD;
+
+module.exports = Cache(`https://${USERNAME}:${PASSWORD}@api.pinboard.in/v1/posts/all?format=json&results=20&tag=like`, {
+  duration: '1d',
+  type: 'json',
+});
