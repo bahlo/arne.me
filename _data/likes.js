039c0e96ffb7ea5a32d7cdc9bb26477757d03ccb
Arne Bahlo <hallo@arne.me>
2020-06-28 15:01:36 +0200

    Fix likes

--- _data/likes.js
+++ _data/likes.js
@@ -1,12 +1,31 @@
 const dotenv = require('dotenv');
-const Cache = require('@11ty/eleventy-cache-assets');
+const { RemoteAssetCache } = require('@11ty/eleventy-cache-assets');
 
 dotenv.config();
 
 const USERNAME = process.env.PINBOARD_USERNAME;
 const PASSWORD = process.env.PINBOARD_PASSWORD;
 
-module.exports = Cache(`https://${USERNAME}:${PASSWORD}@api.pinboard.in/v1/posts/all?format=json&results=20&tag=like`, {
-  duration: '1h',
-  type: 'json',
+// Pinboard started to add spaces before the JSON response, making JSON.parse throw.
+// This class overwrites the getResponseValue and runs trim() on the raw text before parsing.
+class PinboardAssetCache extends RemoteAssetCache {
+	async getResponseValue(response, type) {
+		if (type === "json") {
+			let text = await response.text();
+			return JSON.parse(text.trim());
+		} else if (type === "text") {
+			return response.text();
+		}
+		return response.buffer();
+	}
+}
+
+async function PinboardCache(url, options) {
+	let asset = new PinboardAssetCache(url, options.directory);
+	return asset.fetch(options);
+}
+
+module.exports = PinboardCache(`https://${USERNAME}:${PASSWORD}@api.pinboard.in/v1/posts/all?format=json&results=20&tag=like`, {
+	duration: '1h',
+	type: 'json',
 });
