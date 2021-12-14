2fec8d6dc1f932630003d42f4c75f99775a91d1a
Arne Bahlo <hey@arne.me>
2021-12-14 09:39:28 +0100

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.stdenvNoCC.mkDerivation {
+  name = "arne.me";
+
+  src = with builtins; filterSource (path: type: substring 0 1 
+    (baseNameOf path) != "." && (baseNameOf path) != "default.nix" && (baseNameOf path) != "shell.nix" && type != "symlink")
+    ./.;
+
+  dontConfigure = true;
+  buildInputs = [ pkgs.hugo ];
+  preferLocalBuild = true;
+  installPhase = ''
+    runHook preInstall
+    hugo -d $out
+    runHook postInstall
+  '';
+}
--- shell.nix
+++
@@ -1,7 +0,0 @@
-with import <nixpkgs> {};
-
-mkShell {
-  nativeBuildInputs = with buildPackages; [
-    hugo
-  ];
-}
