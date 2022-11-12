2a982e46fcd89b1abd9db98de05cf1afb43ef15c
Arne Bahlo <hey@arne.me>
2022-11-12 17:27:04 +0100

    Generate pages statically

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -1,11 +1,20 @@
-import { parseMarkdown } from "../../lib/markdown";
+import { buildAbsolutePath, parseMarkdown } from "../../lib/markdown";
+import { promises as fs } from 'node:fs';
 
-export default async function Now() {
-  const { frontmatter, html } = await parseMarkdown('content/now.md');
+export async function generateStaticParams() {
+  const path = buildAbsolutePath('content');
+  const files = await fs.readdir(path);
+  return files.filter(filename => filename.endsWith('.md')).map(filename => ({
+    page: filename.substring(0, filename.length-3),
+  }));
+}
+
+export default async function Page({params: { page }}: { params: { page: string }}) {
+  const { frontmatter, html } = await parseMarkdown('content/'+page+'.md');
 
   return (
     <section>
-      <h1>{frontmatter.hero}</h1>
+      <h1>{frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{__html: html.toString()}}/>
     </section>
   );
