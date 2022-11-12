fe8458937ab8777ef6b23888fd84933bc7eeb81c
Arne Bahlo <hey@arne.me>
2022-11-12 15:42:33 +0100

    Fix home layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { parseMarkdown } from "../lib/markdown";
 export default async function Page() {
   const { frontmatter, html } = await parseMarkdown('content/index.md');
 
-  return (<>
-    <div>
-      <h1>{frontmatter.hero}</h1>
+  return (
+    <section className="intro">
+      <h1 className="title">{frontmatter.hero}</h1>
       <div dangerouslySetInnerHTML={{__html: html.toString()}}/>
-    </div>
-  </>);
+    </section>
+  );
 }
