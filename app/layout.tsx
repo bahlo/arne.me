02127e6ca025a7984c479bae19bad3ecf5b30cb6
Arne Bahlo <hey@arne.me>
2022-11-15 09:13:52 +0100

    Add html lang, set max scale to 5
    This improves accessibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=no"
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#82adf3" />
