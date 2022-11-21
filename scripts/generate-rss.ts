2a8c512edfd932aa87e64d1e3f653611a37b2e1e
Arne Bahlo <hey@arne.me>
2022-11-21 10:07:29 +0100

    Limit RSS feeds to the last 10 items

--- scripts/generate-rss.ts
+++ scripts/generate-rss.ts
@@ -4,12 +4,6 @@ import { getPosts, Post } from "../lib/posts.js";
 import { getIssues, Issue } from "../lib/issues.js";
 
 async function generateBlogFeed() {
-  const blogposts = await getPosts({
-    renderContent: true,
-    renderDescription: false,
-    renderCoverImageCaption: false,
-  });
-
   const feed = new RSS({
     title: "Arne Bahlo — Blog",
     site_url: "https://arne.me",
@@ -17,29 +11,32 @@ async function generateBlogFeed() {
     language: "en",
   });
 
-  blogposts
-    .sort(
-      (a: Post, b: Post) =>
-        b.frontmatter.date.toUnixInteger() - a.frontmatter.date.toUnixInteger()
-    )
-    .forEach((post: Post) => {
-      feed.item({
-        title: post.frontmatter.title,
-        guid: `https://arne.me/blog/${post.slug}`,
-        url: `https://arne.me/blog/${post.slug}`,
-        date: post.frontmatter.date.toJSDate(),
-        description: post.contentHtml!,
-        author: "Arne Bahlo",
-      });
+  let blogposts = await getPosts({
+    renderContent: true,
+    renderDescription: false,
+    renderCoverImageCaption: false,
+  });
+  blogposts = blogposts.sort(
+    (a: Post, b: Post) =>
+      b.frontmatter.date.toUnixInteger() - a.frontmatter.date.toUnixInteger()
+  );
+  blogposts = blogposts.slice(0, 10);
+  blogposts.forEach((post: Post) => {
+    feed.item({
+      title: post.frontmatter.title,
+      guid: `https://arne.me/blog/${post.slug}`,
+      url: `https://arne.me/blog/${post.slug}`,
+      date: post.frontmatter.date.toJSDate(),
+      description: post.contentHtml!,
+      author: "Arne Bahlo",
     });
+  });
 
   const rss = feed.xml();
   await fs.writeFile("./public/blog/atom.xml", rss);
 }
 
 async function generateWeeklyFeed() {
-  const issues = await getIssues({ renderContent: true });
-
   const feed = new RSS({
     title: "Arne’s Weekly",
     site_url: "https://arne.me",
@@ -47,21 +44,22 @@ async function generateWeeklyFeed() {
     language: "en",
   });
 
-  issues
-    .sort(
-      (a: Issue, b: Issue) =>
-        b.frontmatter.date.toUnixInteger() - a.frontmatter.date.toUnixInteger()
-    )
-    .forEach((issue: Issue) => {
-      feed.item({
-        title: issue.frontmatter.title,
-        guid: `https://arne.me/weekly/${issue.num}`,
-        url: `https://arne.me/weekly/${issue.num}`,
-        date: issue.frontmatter.date.toJSDate(),
-        description: issue.contentHtml!,
-        author: "Arne Bahlo",
-      });
+  let issues = await getIssues({ renderContent: true });
+  issues = issues.sort(
+    (a: Issue, b: Issue) =>
+      b.frontmatter.date.toUnixInteger() - a.frontmatter.date.toUnixInteger()
+  );
+  issues = issues.slice(0, 10);
+  issues.forEach((issue: Issue) => {
+    feed.item({
+      title: issue.frontmatter.title,
+      guid: `https://arne.me/weekly/${issue.num}`,
+      url: `https://arne.me/weekly/${issue.num}`,
+      date: issue.frontmatter.date.toJSDate(),
+      description: issue.contentHtml!,
+      author: "Arne Bahlo",
     });
+  });
 
   const rss = feed.xml();
   await fs.writeFile("./public/weekly/atom.xml", rss);
