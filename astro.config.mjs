f72f5cb5c855e5de341b9f3122db5ff882072b6b
Arne Bahlo <hey@arne.me>
2023-03-11 14:49:51 +0100

    Start adding my content to the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://arne.me',
 	integrations: [mdx(), sitemap()],
 });
