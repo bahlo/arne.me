fb95ffde1cfec82765c297ae39fde1ac671d87d8
Arne Bahlo <hey@arne.me>
2023-03-29 10:36:07 +0200

    Add panic function

---
+++ netlify/functions/panic/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	panic("oh no!")
+}
+
+func main() {
+	lambda.Start(handler)
+}
