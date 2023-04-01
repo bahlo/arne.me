b2a23f3df72bdb1f3f5b107684213dc1bc32ee24
Arne Bahlo <hey@arne.me>
2023-04-01 09:07:36 +0200

    Add template for weekly issue content
    We need to have a template to use it in the RSS feed as well.

---
+++ src/templates/WeeklyIssueContent.ts
@@ -0,0 +1,39 @@
+// Astro cannot render components, so we'll have to do this, see
+// https://docs.astro.build/en/reference/errors/invalid-component-args/
+import marked from 'marked';
+
+function buildAnchor(title) {
+  return title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+}
+
+function getHost(url) {
+  const host = new URL(url).host;
+  if (host.startsWith("www.")) {
+    return host.slice(4);
+  }
+  return host;
+}
+
+export default function render(num, frontmatter) {
+  const { tootOfTheWeek, categories } = frontmatter
+
+  // In HTML we trust
+  return `
+    <slot />
+    ${tootOfTheWeek && `
+    <h2 id="toot-of-the-week">Toot of the Week</h2>
+    <blockquote>
+      <p>
+        ${marked.parse(tootOfTheWeek.text)}
+        — <a href="${tootOfTheWeek.url}">${tootOfTheWeek.author}</a>
+      </p>
+    </blockquote>
+    ` || ''}
+    ${(categories || []).map(category => `
+    <h2 id=${buildAnchor(category.title)}>${category.title}</h2>
+    ${category.stories.map(story => `
+    <h3><a href=${"https://click.arne.me/?issue="+num+"&url=" + story.url}>${story.title}</a></h3>
+    <p><em>${story.readingTimeMinutes} min &middot; ${getHost(story.url)}</em></p>
+    ${marked.parse(story.description)}`).join('\n')}`).join('\n')}
+  `
+}
