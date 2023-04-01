45cebc53e69f687006051d8c9f67c2d8e1f0be9a
Arne Bahlo <hey@arne.me>
2023-04-01 14:03:50 +0200

    Convert weekly-issue-to-markdown to TypeScript
    Add readline to prompt for the issue number

---
+++ scripts/weekly-issue-to-markdown.ts
@@ -0,0 +1,67 @@
+import fs from 'fs/promises';
+import matter from 'gray-matter';
+import { cwd } from 'process';
+import readline from 'readline';
+
+function getHost(url: string): string {
+  const host = new URL(url).host;
+  if (host.startsWith("www.")) {
+    return host.slice(4);
+  }
+  return host;
+}
+
+interface Markdown {
+  content: string;
+  data: Frontmatter;
+}
+
+interface Frontmatter {
+  tootOfTheWeek: TootOfTheWeek;
+  categories: Category[];
+}
+
+interface TootOfTheWeek {
+  text: string;
+  author: string;
+  url: string;
+}
+
+interface Category {
+  title: string;
+  stories: Story[];
+}
+
+interface Story {
+  title: string;
+  url: string;
+  readingTimeMinutes: number;
+  description: string;
+}
+
+(async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stderr
+  });
+  const num = await (new Promise(resolve => rl.question("Issue number: ", resolve)));
+  rl.close();
+
+  const raw = await fs.readFile(`${cwd()}/../src/content/weekly/${num}.md`, 'utf-8');
+  const { content, data }: Markdown = matter(raw) as any;
+
+  console.log(content);
+
+  console.log("## Toot of the Weeek");
+  console.log(data.tootOfTheWeek.text.split('\n').map(line => `> ${line}`).join('\n'));
+  console.log(`> — [${data.tootOfTheWeek.author}](${data.tootOfTheWeek.url})`);
+
+  data.categories.forEach(category => {
+    console.log(`## ${category.title}`);
+    category.stories.forEach(story => {
+      console.log(`### [${story.title}](https://click.arne.me/?issue=${num}&url=${story.url})`);
+      console.log(`${story.readingTimeMinutes} min · ${getHost(story.url)}`);
+      console.log(`${story.description}`);
+    });
+  })
+})();
