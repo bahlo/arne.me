d9d6e74ec86e0cb2f1ba9d4228713834cf754fbd
Arne Bahlo <hey@arne.me>
2023-04-04 12:41:37 +0200

    Add og-image for books

---
+++ src/pages/books/[...slug]/og-image.png.ts
@@ -0,0 +1,103 @@
+import satori from "satori";
+import fs from "fs/promises";
+import sharp from "sharp";
+import { getCollection, getEntryBySlug } from "astro:content";
+
+const cooperBt = fs.readFile(
+  "./public/fonts/cooperbt/1296123/d6b715ec-1259-4329-9cfe-5e9d545eea39.woff"
+);
+const roboto = fs.readFile("./public/fonts/roboto/Roboto-Regular.ttf");
+const pattern = fs.readFile("./public/dot-grid.png");
+
+export async function getStaticPaths() {
+  const posts = await getCollection("books");
+  return posts.map((book) => ({
+    params: { slug: book.slug },
+    props: book,
+  }));
+}
+
+export const get: APIRoute = async function get({ params, request }) {
+  const book = await getEntryBySlug("books", params.slug);
+
+  const date = book.data.dateRead.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  const cooperBtData = await cooperBt;
+  const robotoData = await roboto;
+  const base64Pattern = (await pattern).toString("base64");
+
+  const svg = await satori(
+    {
+      type: "div",
+      props: {
+        children: [
+          {
+            type: "h1",
+            props: {
+              children: book.data.title,
+              style: {
+                fontFamily: "Cooper BT",
+                fontSize: "64px",
+                lineHeight: 1,
+                color: "#111",
+                marginBottom: "32px",
+              },
+            },
+          },
+          {
+            type: "span",
+            props: {
+              children: `${book.data.author} · Read on ${date}`,
+              style: {
+                color: "#333",
+                fontFamily: "Roboto",
+                fontSize: "40px",
+              },
+            },
+          },
+        ],
+        style: {
+          width: 1200,
+          height: 630,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          borderBottom: "20px solid rgb(53, 88, 255)",
+          backgroundImage: `url('data:image/png;base64,${base64Pattern}')`,
+          backgroundRepeat: "repeat",
+          padding: "80px",
+        },
+      },
+    },
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Cooper BT",
+          data: cooperBtData,
+          weight: "bold",
+          style: "normal",
+        },
+        {
+          name: "Roboto",
+          data: robotoData,
+          weight: "normal",
+          style: "normal",
+        },
+      ],
+    }
+  );
+
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
+
+  return new Response(png, {
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+};
