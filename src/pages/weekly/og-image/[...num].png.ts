3ba19482f5ad84ba6c0dcc543e69d5e23d219bb7
Arne Bahlo <hey@arne.me>
2023-04-04 09:56:07 +0200

    Move ogImage logic to shared/renderOgImage

--- src/pages/weekly/og-image/[...num].png.ts
+++ src/pages/weekly/og-image/[...num].png.ts
@@ -1,8 +1,5 @@
-import satori from "satori";
-import fs from "fs/promises";
-import type { APIRoute } from "astro";
-import sharp from "sharp";
 import { getCollection, getEntryBySlug } from "astro:content";
+import renderOgImage from "../../../shared/renderOgImage.ts";
 
 export async function getStaticPaths() {
   const issues = await getCollection("weekly");
@@ -12,17 +9,7 @@ export async function getStaticPaths() {
   }));
 }
 
-const cooperBt = fs.readFile(
-  "./public/fonts/cooperbt/1296123/d6b715ec-1259-4329-9cfe-5e9d545eea39.woff"
-);
-const roboto = fs.readFile("./public/fonts/roboto/Roboto-Regular.ttf");
-const pattern = fs.readFile("./public/dot-grid.png");
-
 export const get: APIRoute = async function get({ params, request }) {
-  const cooperBtData = await cooperBt;
-  const robotoData = await roboto;
-  const base64Pattern = (await pattern).toString("base64");
-
   const issue = await getEntryBySlug("weekly", params.num);
 
   const date = issue.data.date.toLocaleDateString("en-us", {
@@ -30,75 +17,7 @@ export const get: APIRoute = async function get({ params, request }) {
     month: "short",
     day: "numeric",
   });
+  const description = `Arne's Weekly from ${date}.`;
 
-  const svg = await satori(
-    {
-      type: "div",
-      props: {
-        children: [
-          {
-            type: "h1",
-            props: {
-              children: issue.data.title,
-              style: {
-                fontFamily: "Cooper BT",
-                fontSize: "64px",
-                lineHeight: 1,
-                color: "#111",
-                marginBottom: "32px",
-              },
-            },
-          },
-          {
-            type: "span",
-            props: {
-              children: `Arne's Weekly from ${date}.`,
-              style: {
-                color: "#333",
-                fontFamily: "Roboto",
-                fontSize: "40px",
-              },
-            },
-          },
-        ],
-        style: {
-          width: 1200,
-          height: 630,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          borderBottom: "20px solid rgb(53, 88, 255)",
-          backgroundImage: `url('data:image/png;base64,${base64Pattern}')`,
-          backgroundRepeat: "repeat",
-          padding: "80px",
-        },
-      },
-    },
-    {
-      width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: "Cooper BT",
-          data: cooperBtData,
-          weight: "bold",
-          style: "normal",
-        },
-        {
-          name: "Roboto",
-          data: robotoData,
-          weight: "normal",
-          style: "normal",
-        },
-      ],
-    }
-  );
-
-  const png = await sharp(Buffer.from(svg)).png().toBuffer();
-
-  return new Response(png, {
-    headers: {
-      "Content-Type": "image/png",
-    },
-  });
+  return renderOgImage(issue.data.title, description);
 };
