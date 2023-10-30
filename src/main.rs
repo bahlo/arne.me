133e848aff9ec70b2f28e382dc94b98646dba77a
Arne Bahlo <hey@arne.me>
2023-10-30 09:57:56 +0100

    feat(server): Add tracing crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use axum::{routing::get, Router, Server};
+use tracing::info;
+use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
 
 mod layout;
 mod routes;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
     let app = Router::new().route("/", get(routes::index));
 
+    info!(port = 3000, "Starting server");
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await?;
