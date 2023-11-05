c0d10871a235938d7e5ebd8f979b2f622610efad
Arne Bahlo <hey@arne.me>
2023-11-05 18:35:23 +0100

    feat(styles): Add download-fonts script

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use anyhow::{anyhow, bail, Result};
 use clap::Parser;
-use std::{fs, path::Path};
+use std::{
+    env,
+    fs::{self, File},
+    io,
+    path::Path,
+};
+use tempdir::TempDir;
+use zip::ZipArchive;
 
 mod content;
 mod layout;
@@ -22,6 +29,8 @@ enum Commands {
     Build,
     #[clap(name = "export-weekly")]
     ExportWeekly { num: u16 },
+    #[clap(name = "download-fonts")]
+    DownloadFonts,
 }
 
 fn main() -> Result<()> {
@@ -30,6 +39,7 @@ fn main() -> Result<()> {
     match cli.command {
         Commands::Build => build(),
         Commands::ExportWeekly { num } => export_weekly(num),
+        Commands::DownloadFonts => download_fonts(),
     }
 }
 
@@ -209,5 +219,50 @@ fn export_weekly(num: u16) -> Result<()> {
         println!();
     });
 
+    Ok(())
+}
+
+fn download_fonts() -> Result<()> {
+    let zip_url = env::var("FONT_ZIP_URL")?;
+    let destination = Path::new("./static/fonts");
+
+    let response = ureq::get(&zip_url).call()?;
+    let mut reader = response.into_reader();
+
+    let temp_dir = TempDir::new("arne-me-fonts")?;
+    let zip_path = temp_dir.path().join("fonts.zip");
+    let mut temp_file = File::create(&zip_path)?;
+    io::copy(&mut reader, &mut temp_file)?;
+
+    let zip_file = File::open(&zip_path)?;
+    let mut archive = ZipArchive::new(zip_file)?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let outpath = match file.enclosed_name() {
+            Some(path) => destination.join(path),
+            None => continue,
+        };
+
+        if file.name() == "__MACOSX" {
+            // Skip macOS metadata
+            continue;
+        }
+
+        if file.name().ends_with('/') {
+            fs::create_dir_all(&outpath)?;
+        } else {
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p)?;
+                }
+            }
+
+            let mut outfile = File::create(&outpath)?;
+            io::copy(&mut file, &mut outfile)?;
+        }
+    }
+
+    temp_dir.close()?;
     Ok(())
 }
