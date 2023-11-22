34ba9b60fcdc01e5b209f2e5e4d6c4e9dde784fe
Arne Bahlo <hey@arne.me>
2023-11-22 21:24:50 +0100

    feat(static): Add favicons

--- src/templates/layout.rs
+++ src/templates/layout.rs
@@ -102,6 +102,10 @@ impl Layout {
                     link rel="preload" href="/fonts/rebond-grotesque/ESRebondGrotesque-Regular.woff2" as="font" type="font/woff2";
                     link rel="preload" href="/fonts/rebond-grotesque/ESRebondGrotesque-Bold.woff2" as="font" type="font/woff2";
                     link rel="preload" href="/fonts/rebond-grotesque/ESRebondGrotesque-Italic.woff2" as="font" type="font/woff2";
+                    link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png";
+                    link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png";
+                    link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png";
+                    link rel="manifest" href="/site.webmanifest";
 
                     @if let Some(port) = self.hot_reload_websocket_port {
                         script {
