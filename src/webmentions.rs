f5f79bd583aea385ec3db274400545fb429253f3
Arne Bahlo <hey@arne.me>
2023-11-25 22:28:54 +0100

    feat(webmentions): Add article support to send-webmentions
    Don't @ me about the html regex

--- src/webmentions.rs
+++ src/webmentions.rs
@@ -1,12 +1,15 @@
 use crate::content::Content;
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use lazy_static::lazy_static;
+use regex::Regex;
 use scraper::{Html, Selector};
 use std::fs;
 
 lazy_static! {
     pub static ref SELECTOR: Selector =
         Selector::parse(r#"link[rel="webmention"]"#).expect("Failed to parse webmention selector");
+    pub static ref LINK_REGEX: Regex =
+        Regex::new(r#"(https?://[^"]+)"#).expect("Failed to parse link regex");
 }
 
 pub fn send_webmentions(path: impl AsRef<str>, dry_run: bool) -> Result<()> {
@@ -19,6 +22,7 @@ pub fn send_webmentions(path: impl AsRef<str>, dry_run: bool) -> Result<()> {
 
     match kind {
         "weekly" => send_webmentions_weekly(dry_run, content, slug)?,
+        "articles" => send_webmentions_article(dry_run, content, slug)?,
         _ => bail!("Invalid kind, expected 'weekly'"),
     }
 
@@ -39,21 +43,43 @@ fn send_webmentions_weekly(dry_run: bool, content: Content, slug: impl AsRef<str
 
     for category in weekly_issue.categories.iter() {
         for story in category.stories.iter() {
-            if let Err(e) = send_webmention(
+            send_webmention(
                 dry_run,
                 &format!("https://arne.me/weekly/{}", num),
                 &story.url,
-            ) {
-                eprintln!("Failed to send webmention for {}: {}", &story.url, e);
-            }
+            )
+            .unwrap_or_else(|e| eprintln!("Failed to send webmention for {}: {}", &story.url, e))
         }
     }
 
     Ok(())
 }
 
+fn send_webmentions_article(dry_run: bool, content: Content, slug: impl AsRef<str>) -> Result<()> {
+    let slug = slug.as_ref();
+    let article = content
+        .articles
+        .iter()
+        .find(|article| article.slug == slug)
+        .ok_or_else(|| anyhow!("Article not found"))?;
+
+    LINK_REGEX
+        .captures_iter(&article.content_html)
+        .for_each(|capture| {
+            let url = capture.get(1).unwrap().as_str();
+            send_webmention(dry_run, format!("https://arne.me/articles/{}", slug), url)
+                .unwrap_or_else(|e| println!("Failed to send webmention for {}: {}", url, e));
+        });
+
+    Ok(())
+}
+
 fn send_webmention(dry_run: bool, source: impl AsRef<str>, target: impl AsRef<str>) -> Result<()> {
-    let html = ureq::get(target.as_ref()).call()?.into_string()?;
+    let html = ureq::get(target.as_ref())
+        .call()
+        .context("Failed to get HTML")?
+        .into_string()
+        .context("Failed to get String from response")?;
     let document = Html::parse_document(&html);
     let endpoint = document
         .select(&SELECTOR)
