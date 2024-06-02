c92e2c3ade5b1c4a0c4a8266543546af033df8a6
Arne Bahlo <hey@arne.me>
2024-06-02 14:37:21 +0200

    feat(export-weekly-opml): Write OPML to stdout

--- src/export_weekly_opml.rs
+++ src/export_weekly_opml.rs
@@ -1,14 +1,18 @@
 use anyhow::{Context, Result};
+use chrono::{DateTime, Utc};
 use lazy_static::lazy_static;
 use scraper::{Html, Selector};
+use serde::Serialize;
 use std::{
     collections::HashSet,
-    fmt, fs,
+    fs,
     hash::{Hash, Hasher},
 };
 use url::Url;
 
 use crate::content::Content;
+
+const XML_DECLARATION: &'static str = r#"<?xml version="1.0" encoding="UTF-8"?>"#;
 
 lazy_static! {
     pub static ref SELECTOR: Selector =
@@ -26,10 +30,13 @@ pub fn export_weekly_opml() -> Result<()> {
                 .iter()
                 .flat_map(|category| category.stories.iter().map(|story| story.url.clone()))
         })
-        .flat_map(|url| match fetch_feed_urls(url.clone()) {
-            Ok(feed_urls) => feed_urls,
+        .flat_map(|url| match fetch_feeds(url.clone()) {
+            Ok(feed_urls) => {
+                eprint!(".");
+                feed_urls
+            }
             Err(e) => {
-                eprintln!("Failed to fetch feeds for {}: {}", url, e);
+                eprintln!("\nFailed to fetch feeds for {}: {}", url, e);
                 return vec![];
             }
         })
@@ -37,36 +44,52 @@ pub fn export_weekly_opml() -> Result<()> {
             set.insert(feed);
             set
         });
+    eprintln!("\n");
 
-    feeds.iter().for_each(|feed| println!("{}", feed));
+    let opml = Opml {
+        version: "1.0".to_string(),
+        head: Head {
+            title: "RSS feeds from stories in all Arne's Weekly issues".to_string(),
+            date_created: Utc::now(),
+        },
+        body: Body {
+            outline: feeds.iter().map(|feed| feed.into()).collect(),
+        },
+    };
 
+    let mut xml = String::new();
+    xml.push_str(XML_DECLARATION);
+    xml.push_str(&quick_xml::se::to_string(&opml)?);
+
+    println!("{}", xml);
     Ok(())
 }
 
 #[derive(Debug, Eq, PartialEq)]
 struct Feed {
     title: Option<String>,
-    url: Url,
+    feed_url: Url,
+    html_url: Url,
 }
 
 impl Hash for Feed {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.url.hash(state);
+        self.feed_url.hash(state);
     }
 }
 
-impl fmt::Display for Feed {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(title) = self.title.as_deref() {
-            write!(f, "{}: {}", title, self.url)
-        } else {
-            write!(f, "{}", self.url)
+impl From<&Feed> for Outline {
+    fn from(value: &Feed) -> Self {
+        Outline {
+            text: value.title.clone().unwrap_or_default(),
+            typ: "rss".to_string(),
+            xml_url: value.feed_url.clone(),
         }
     }
 }
 
-fn fetch_feed_urls(site: Url) -> Result<Vec<Feed>> {
-    let html = ureq::get(site.as_ref())
+fn fetch_feeds(site_url: Url) -> Result<Vec<Feed>> {
+    let html = ureq::get(site_url.as_ref())
         .call()
         .context("Failed to get HTML")?
         .into_string()
@@ -88,14 +111,67 @@ fn fetch_feed_urls(site: Url) -> Result<Vec<Feed>> {
                 .value()
                 .attr("href")
                 .context("Missing href attribute")?;
-            let url = if url_attr.starts_with("/") {
-                site.join(url_attr)
+            let feed_url = if url_attr.starts_with("/") {
+                site_url.join(url_attr)
             } else {
                 Url::parse(url_attr)
             }?;
 
             let title = element.value().attr("title").map(|s| s.to_string());
-            Ok(Feed { title, url })
+            Ok(Feed {
+                title,
+                feed_url,
+                html_url: site_url.clone(),
+            })
         })
         .collect()
+}
+
+#[derive(Serialize)]
+#[serde(rename = "opml")]
+struct Opml {
+    #[serde(rename = "@version")]
+    version: String,
+    head: Head,
+    body: Body,
+}
+
+#[derive(Serialize)]
+#[serde(rename = "head")]
+struct Head {
+    title: String,
+    #[serde(with = "rfc_822")]
+    date_created: DateTime<Utc>,
+}
+
+#[derive(Serialize)]
+#[serde(rename = "body")]
+struct Body {
+    outline: Vec<Outline>,
+}
+
+#[derive(Serialize)]
+#[serde(rename = "outline")]
+struct Outline {
+    #[serde(rename = "@text")]
+    text: String,
+    #[serde(rename = "@type")]
+    typ: String,
+    #[serde(rename = "@xmlUrl")]
+    xml_url: Url,
+}
+
+mod rfc_822 {
+    use chrono::{DateTime, Utc};
+    use serde::{self, Serializer};
+
+    const FORMAT: &str = "%a, %d %b %Y %H:%M:%S %z";
+
+    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
+    }
 }
