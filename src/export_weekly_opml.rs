fda201defdf25b030d1071ecacbc73328a1622c9
Arne Bahlo <hey@arne.me>
2024-06-02 15:12:10 +0200

    fix(export-weekly-opml): Add user agent
    Some pages block based on user-agent, but we're not doing anything bad!

--- src/export_weekly_opml.rs
+++ src/export_weekly_opml.rs
@@ -89,6 +89,7 @@ impl From<&Feed> for Outline {
 
 fn fetch_feeds(site_url: Url) -> Result<Vec<Feed>> {
     let html = ureq::get(site_url.as_ref())
+        .set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15")
         .call()
         .context("Failed to get HTML")?
         .into_string()
