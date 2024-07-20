a58a6c96b8a087719ecaae4807d079f4497363c6
Arne Bahlo <hey@arne.me>
2024-07-20 22:48:51 +0200

    fix(design): Add theme color

--- src/templates/layout.rs
+++ src/templates/layout.rs
@@ -96,7 +96,7 @@ impl Layout {
                     meta name="title" content=(smart_quotes(head.title.clone()));
                     meta name="description" content=(smart_quotes(head.description.clone()));
                     meta name="author" content="Arne Bahlo";
-                    // meta name="theme-color" content="#010101";
+                    meta name="theme-color" content="#4040ff";
                     meta name="viewport" content="width=device-width,initial-scale=1";
                     meta property="og:type" content=(head.og_type);
                     meta property="og:url" content=(head.url);
