d698e02c11eabb64e5b6348f1e6471b1cc88a6a0
Arne Bahlo <hey@arne.me>
2024-07-21 11:11:23 +0200

    feat(book-reviews): Show covers on overview

--- src/templates/book_review.rs
+++ src/templates/book_review.rs
@@ -27,9 +27,9 @@ pub fn render(book_review: &BookReview) -> Result<Context> {
             og_type: OgType::Article,
         },
         html! {
-            article.book_review.h-entry {
+            article.book_reviews.h-entry {
                 div {
-                    h1.p-name.book_review__heading { (book_review.title) " by " (book_review.author) }
+                    h1.p-name { (book_review.title) " by " (book_review.author) }
                     a.u-url hidden { (format!("/book-reviews/{}", book_review.slug)) }
                     span.p-summary hidden { (format!("I read {} by {}", book_review.title, book_review.author)) }
                     span.p-author hidden { "Arne Bahlo" }
@@ -44,7 +44,7 @@ pub fn render(book_review: &BookReview) -> Result<Context> {
                 }
                 picture {
                     source srcset=(format!("/book-reviews/{}/cover.avif", book_review.slug)) type="image/avif";
-                    img.book_review__cover src=(format!("/book-reviews/{}/cover.jpg", book_review.slug)) alt=(format!("The cover of {} by {}", book_review.title, book_review.author));
+                    img.book_reviews__cover src=(format!("/book-reviews/{}/cover.jpg", book_review.slug)) alt=(format!("The cover of {} by {}", book_review.title, book_review.author));
                 }
                 .e-content {
                     (PreEscaped(book_review.content_html.clone()))
@@ -68,20 +68,16 @@ pub fn render_index(content: &Content) -> Result<Context> {
         html! {
             section.book_reviews {
                 h1 { "Book reviews" }
-                @for book_review in &content.book_reviews {
-                    div {
-                        h3.book_review__heading  {
-                                a href=(format!("/book-reviews/{}", book_review.slug)) {
-                                    (book_review.title) " by " (book_review.author)
-                                }
-                        }
-                        i.byline {
-                            time datetime=(book_review.read.format("%Y-%m-%d")) { (format_date(book_review.read)) }
-                            (PreEscaped(" &middot; "))
-                            (book_review.location)
-                            (PreEscaped(" &middot; "))
-                            (book_review.rating)
-                            "/5"
+                p {
+                    "Since March 2023 I'm writing book reviews for all books I'm reading:"
+                }
+                .book_reviews__grid {
+                    @for book_review in &content.book_reviews {
+                        a href=(format!("/book-reviews/{}", book_review.slug)) {
+                            picture {
+                                source srcset=(format!("/book-reviews/{}/cover.avif", book_review.slug)) type="image/avif";
+                                img src=(format!("/book-reviews/{}/cover.jpg", book_review.slug)) alt=(format!("The cover of {} by {}", book_review.title, book_review.author));
+                            }
                         }
                     }
                 }
