c0838eb4fa12bd40301bde0e4bf214df606e6979
Arne Bahlo <hey@arne.me>
2024-10-27 14:05:41 +0100

    Add logic to wait for pages to return 200

--- crates/cli/src/syndicate.rs
+++ crates/cli/src/syndicate.rs
@@ -1,4 +1,6 @@
-use anyhow::Result;
+use std::{thread::sleep, time::Duration};
+
+use anyhow::{bail, Result};
 use git2::{Delta, DiffDelta, Oid, Repository};
 
 fn syndicate_diff_cb(diff_delta: DiffDelta<'_>, _i: f32) -> bool {
@@ -57,9 +59,33 @@ pub fn syndicate_before_sha(before_sha: String) -> Result<()> {
     Ok(())
 }
 
+pub fn wait_for_200(slug: String) -> Result<()> {
+    let url = format!("https://arne.me/{slug}");
+    println!("Waiting for {url} to return HTTP 200");
+    for i in 0..300 {
+        // 5 mins
+        match ureq::get(&url).call() {
+            Ok(res) => {
+                if res.status() == 200 {
+                    return Ok(());
+                } else {
+                    eprintln!("Received HTTP {}, retrying in 1s ({i}/300)", res.status());
+                }
+            }
+            Err(e) => {
+                eprintln!("Request failed: {e}, retrying in 1s ({i}/300)");
+            }
+        };
+        sleep(Duration::from_secs(1))
+    }
+
+    bail!("Failed to reach {url} in 5 minutes")
+}
+
 pub fn syndicate_slug(slug: impl Into<String>) -> Result<()> {
     let slug = slug.into();
     println!("Syndicating {slug}...");
+    wait_for_200(slug)?;
     // TODO
     Ok(())
 }
