efd7c1421ed8caee2272608b34ce3d2397ad0744
Arne Bahlo <hey@arne.me>
2024-10-27 13:57:54 +0100

    Move syndicate logic to extra mod

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,14 +1,15 @@
 use anyhow::{anyhow, bail, Result};
 use chrono::Utc;
 use clap::Parser;
-use git2::{Delta, DiffDelta, Oid, Repository};
 use std::{env, fs, path::Path, process::Command};
 
 mod og;
+mod syndicate;
 mod watch;
 mod webmentions;
 
 use arneos::content::{Content, Item};
+use syndicate::{syndicate_before_sha, syndicate_slug};
 use webmentions::send_webmentions;
 
 #[derive(Debug, Parser)]
@@ -235,68 +236,5 @@ fn new_og_image(path: impl AsRef<str>) -> Result<()> {
             }
         }
     }
-    Ok(())
-}
-
-fn syndicate_diff_cb(diff_delta: DiffDelta<'_>, _i: f32) -> bool {
-    if diff_delta.status() != Delta::Added {
-        return true; // continue
-    }
-
-    let filepath = match diff_delta.new_file().path() {
-        Some(filepath) => filepath,
-        None => {
-            eprintln!(
-                "Failed to get the path of one of the new files: {:?}",
-                diff_delta.new_file()
-            );
-            return true;
-        }
-    };
-
-    if filepath.extension().and_then(|s| s.to_str()) != Some("md") {
-        // Not a markdown file, "continue"
-        return true;
-    }
-
-    let slug = match filepath
-        .strip_prefix("content/")
-        .map(|path| path.with_extension(""))
-    {
-        Ok(slug) => slug,
-        Err(e) => {
-            eprintln!("Failed to strip the 'content/' prefix: {e}");
-            return true;
-        }
-    };
-
-    if let Err(e) = syndicate_slug(slug.to_string_lossy()) {
-        eprintln!("Failed to syndicate {slug:?}: {e}");
-    };
-
-    true // continue
-}
-
-fn syndicate_before_sha(before_sha: String) -> Result<()> {
-    let repo = Repository::open(".")?;
-
-    let head = repo.head()?;
-    let head_tree = head.peel_to_tree()?;
-
-    let before_commit_oid = Oid::from_str(&before_sha)?;
-    let before_commit = repo.find_commit(before_commit_oid)?;
-    let before_commit_tree = before_commit.tree()?;
-
-    let diff = repo.diff_tree_to_tree(Some(&before_commit_tree), Some(&head_tree), None)?;
-
-    diff.foreach(&mut syndicate_diff_cb, None, None, None)?;
-
-    Ok(())
-}
-
-fn syndicate_slug(slug: impl Into<String>) -> Result<()> {
-    let slug = slug.into();
-    println!("Syndicating {slug}...");
-    // TODO
     Ok(())
 }
